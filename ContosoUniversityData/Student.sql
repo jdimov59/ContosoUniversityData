d716e8ade855ab5159f40142b606ab5af16e12c3
jdimov59 <jdimov59@gmail.com>
2016-06-10 18:09:39 +0300

    EF Database First with ASP.NET MVC: Changing the Database
    Add a column to the Student table
    Update Model from Database
    Update StudentsController and Views

--- ContosoUniversityData/Student.sql
+++ ContosoUniversityData/Student.sql
@@ -3,6 +3,7 @@
 	[StudentID] INT IDENTITY (1, 1) NOT NULL,
 	[LastName]  NVARCHAR (50) NULL,
 	[FirstName] nvarchar (50) null,
+	[MiddleName] nvarchar (50) null,
 	[EnrollmentDate] datetime null,
 	PRIMARY KEY clustered ([StudentID] ASC)
 )
